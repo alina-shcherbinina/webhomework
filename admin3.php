fa08de39e2ee66dfcdaa7c974e636e4bc9aefa8d
Алина Щербинина <48168698+alina-shcherbinina@users.noreply.github.com>
2020-06-11 13:44:05 +0800

    Update admin3.php

--- admin3.php
+++ admin3.php
@@ -1,5 +1,5 @@
 <?php
-mb_internal_encoding('UTF-8');
+
 include 'autoload.php';
 
 use classes\order;
@@ -8,36 +8,12 @@ if ($_POST)
 {
 	if (!empty($_POST['selected_ids'])) 
   {
-		$contents = file_get_contents('data.txt');
-		$contents = trim($contents);
-
-		$items = explode("\n", $contents);
-
-		$new_contents = [];
-
-		foreach ($items as $item)
-		{
-			$shouldBeDeleted = false;
-			foreach ($_POST['selected_ids'] as $id) 
-			{
-				$item=trim($item);
-				$cols=explode('||', $item);
-				if ($cols[0]===$id)
-        {
-					$shouldBeDeleted=true;
-				}
-			}
-			if (!$shouldBeDeleted) 
-			{
-				$new_contents[]=$item;
-			}
-		}
-		file_put_contents('data.txt', implode("\n", $new_contents) . "\n");
-	}
+  	foreach ($_POST['selected_ids'] as $id) {
+  		order::delete($id);
+  	}
+  }
 }
 
-$order= new order;
-
-$data= $order->getData('data.txt');
-
+$data=order::loadAll();
+var_dump($data);
 include 'templates/admin.php';
